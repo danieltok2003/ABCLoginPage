550299a0e3be0c01283cd4ed94045dacdaa0a2f8
danieltok2003 <danieltok2003@gmail.com>
2023-07-13 10:08:19 +0800

    pre database migration

--- roomBooking/roomBooking/settings.py
+++ roomBooking/roomBooking/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 import os
 
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -85,13 +87,25 @@ WSGI_APPLICATION = 'roomBooking.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+     'default': {
+        'NAME': 'my_database',
+        'ENGINE': 'sqlserver_ado',
+        'HOST': 'dbserver\\ss2012',
+        'USER': '',
+        'PASSWORD': '',
     }
 }
+
+
+
 
 
 # Password validation
@@ -132,9 +146,9 @@ USE_TZ = True
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, "static"),
+# )
 MEDIA_ROOT = BASE_DIR
 MEDIA_URL = "/media/"
 
